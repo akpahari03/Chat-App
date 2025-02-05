df16997acbed719536c5e007df665c8c7f779780
Ayush Kumar Pahari <ayush@Ayushs-MacBook-Air.local>
2025-02-05 18:33:50 +0530

    v4

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // Ensure all relevant file extensions are included
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
