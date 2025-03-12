e5d9e27789d8cf1d6c9b245dd2654437dcdcd20e
Ayush Kumar Pahari <103330068+akpahari03@users.noreply.github.com>
2025-03-12 14:08:41 +0530

    Delete frontend directory

--- frontend/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"], // Ensure all relevant file extensions are included
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
